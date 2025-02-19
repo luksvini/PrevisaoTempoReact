ed6c81dda602d7db17749f12be17228a8ece1785
Lucas <vinlucaz9@gmail.com>
2025-02-19 09:37:07 -0300

    novos ajustes

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 import "dotenv/config"
 
 
-const URL = import.meta.env.VITE_API_URL || "/api"
+const URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api"
 
  const response = await axios.get(`${URL}/clima`)
 
