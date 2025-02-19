fb303be039d456d3434b622d04115d6a8aa4c184
Lucas <vinlucaz9@gmail.com>
2025-02-19 10:31:09 -0300

    novos ajustes

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -3,10 +3,15 @@ import axios from "axios"
 // import "dotenv/config"
 
 // const { NODE_ENV } = process.env
- const URL = "http://localhost:5000/api"
-
-//const URL = "https://previsaotemporeact.onrender.com/api"
-
- const response = await axios.get(`${URL}/clima`)
+//  const URL = "http://localhost:5000/api"
+const API_URL = import.meta.env.VITE_API_URL;
 
 
+
+export const fetchWeather = async (cidade) => {
+    return axios.get(`${API_URL}/clima?cidade=${cidade}`);
+  };
+  
+  export const fetchWeather5Days = async (cidade) => {
+    return axios.get(`${API_URL}/clima5dias?cidade=${cidade}`);
+  };
